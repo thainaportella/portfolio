f74f0e0ad1d4d9c58a37451481c878b4ed15fc5c
Thainá Rodrigues <mr.thaaina@gmail.com>
2022-08-31 12:18:06 -0300

    Permite clicar em qualquer local do menu dropdown, não apenas onde está escrito

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const menuButton = document.querySelector('.menu-button')
 const menuList = document.querySelector('.menu')
-const dropdownIndex = document.querySelector('.index')
-const dropdownAbout = document.querySelector('.about')
-const dropdownPortfolio = document.querySelector('.portfolio')
-const dropdownContact = document.querySelector('.contact')
 
 const openMenu = () => {
   if (!menuList.classList.contains('clicked')) {
     menuList.classList.add('clicked')
-  } else {
-    closeMenu()
-  }
+  } else { closeMenu() }
 }
 
 const closeMenu = () => {
@@ -18,7 +12,3 @@ const closeMenu = () => {
 }
 
 menuButton.addEventListener('click', () => openMenu())
-dropdownIndex.addEventListener('click', () => closeMenu())
-dropdownAbout.addEventListener('click', () => closeMenu())
-dropdownPortfolio.addEventListener('click', () => closeMenu())
-dropdownContact.addEventListener('click', () => closeMenu())
