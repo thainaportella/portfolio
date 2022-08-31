ea98c8b8fcd3f3af8b76a70dbbeec779a584e7d2
Thainá Rodrigues <mr.thaaina@gmail.com>
2022-08-31 11:44:24 -0300

    Menu responsivo fecha quando opção é clicada

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const menuButton = document.querySelector('.menu-button')
 const menuList = document.querySelector('.menu')
+const dropdownIndex = document.querySelector('.index')
+const dropdownAbout = document.querySelector('.about')
+const dropdownPortfolio = document.querySelector('.portfolio')
+const dropdownContact = document.querySelector('.contact')
 
 const openMenu = () => {
-  menuList.classList.toggle('clicked')
+  if (!menuList.classList.contains('clicked')) {
+    menuList.classList.add('clicked')
+  } else {
+    closeMenu()
+  }
+}
+
+const closeMenu = () => {
+  menuList.classList.remove('clicked')
 }
 
 menuButton.addEventListener('click', () => openMenu())
+dropdownIndex.addEventListener('click', () => closeMenu())
+dropdownAbout.addEventListener('click', () => closeMenu())
+dropdownPortfolio.addEventListener('click', () => closeMenu())
+dropdownContact.addEventListener('click', () => closeMenu())
